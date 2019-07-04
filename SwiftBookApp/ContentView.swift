42f63de8a47411cf3c03b60f8a3dd4aa84a668d9
jack sp@rrow <stepanet@gmail.com>
2019-07-05 02:02:26 +0300

    aa

--- SwiftBookApp/ContentView.swift
+++ SwiftBookApp/ContentView.swift
@@ -23,7 +23,7 @@ struct ContentView : View {
                     
                     } .listRowInsets(EdgeInsets())
                 
-                NavigationButton(destination: FriendsList()) {
+                NavigationLink(destination: FriendsList()) {
                     Text("Наши преподователи")
                 }
                 }.navigationBarTitle(Text("Homepage"))
